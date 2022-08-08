72c0bd0d76ef30548184f7ab732383be0f6dfdac
Shine <50844675+Shine0064@users.noreply.github.com>
2022-08-08 22:22:27 +0200

    Fixed loadstrings breaking.

--- MidnightLib.lua
+++ MidnightLib.lua
@@ -1,1 +1,1 @@
-loadstring(http.request({Url = "https://raw.githubusercontent.com/Shine0064/UI-Libraries/main/Midnight/MidnightLib.lua", Method = "GET"}).Body)()
+return loadstring(http.request({Url = "https://raw.githubusercontent.com/Shine0064/UI-Libraries/main/Midnight/MidnightLib.lua", Method = "GET"}).Body)()
